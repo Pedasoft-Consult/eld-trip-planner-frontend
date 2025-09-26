85d0d0e732e713947f866264d0e3f0db33b41808
Pedahel Emmanuel Kojo <118267361+Pedasoft-Consult@users.noreply.github.com>
2025-09-26 21:54:41 +0000

    Fix this codes

---
+++ env.d.ts
@@ -0,0 +1,10 @@
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string
+  readonly VITE_MAPBOX_TOKEN: string
+  readonly VITE_APP_NAME: string
+  readonly VITE_APP_VERSION: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
